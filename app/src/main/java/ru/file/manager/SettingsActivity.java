f3d4cc864cfcfce4d8d953faa9da3e23992badc5
elmurzaev <elmurzaev.ram@gmail.com>
2019-06-09 23:08:26 +0300

    Improvements

--- app/src/main/java/ru/file/manager/SettingsActivity.java
+++ app/src/main/java/ru/file/manager/SettingsActivity.java
@@ -1,11 +1,9 @@
 package ru.file.manager;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,18 +12,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        if (toolbar != null) {
-            toolbar.setNavigationIcon(R.drawable.ic_back);
-            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    SettingsActivity.this.finish();
-                }
-            });
-            toolbar.setTitle(R.string.navigation_settings);
-        }
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment, new SettingsFragment()).commit();
         setSupportActionBar(toolbar);
     }
 
